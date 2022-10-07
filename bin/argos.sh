be280d236723c0f339844d2fa1cd0376cce80a62
fzaninotto <fzaninotto@gmail.com>
2022-10-07 10:35:36 +0200

    This code has moved

--- bin/argos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$DIR/.."
-
-# shellcheck disable=SC2068
-node dist/index.js $@
--- src/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-    interface ProcessEnv {
-        NAME: string;
-    }
-}
-
-declare module "js-yaml" {
-    export function safeLoad<T = unknown>(value: string): T;
-}
